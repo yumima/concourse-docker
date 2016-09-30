31ca8905e19f38d6b3a7c89219a6f8b56dd46863
Yuming Ma <yumima@cisco.com>
2016-09-30 14:56:34 -0700

    check in

---
+++ dci.sh
@@ -0,0 +1,16 @@
+# copy and run these commands on local dir
+mkdir dci
+cd dci
+mkdir -p keys/web keys/worker
+
+ssh-keygen -t rsa -f ./keys/web/tsa_host_key -N ''
+ssh-keygen -t rsa -f ./keys/web/session_signing_key -N ''
+
+ssh-keygen -t rsa -f ./keys/worker/worker_key -N ''
+
+cp ./keys/worker/worker_key.pub ./keys/web/authorized_worker_keys
+cp ./keys/web/tsa_host_key.pub ./keys/worker
+
+export CONCOURSE_EXTERNAL_URL=http://104.198.97.102
+
+docker-compose up -d
