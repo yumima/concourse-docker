da2a8d2d0efd9abb9caab4e00463acb16fc4e17c
Yuming Ma <yumima@cisco.com>
2016-10-04 17:32:24 -0700

    check in playbook for cci server creation

---
+++ env.sh
@@ -0,0 +1,3 @@
+export GCE_EMAIL=712447643318-compute@developer.gserviceaccount.com
+export GCE_CREDENTIALS_FILE_PATH=/home/yumima/keys/3cbb462c342002e58105f68ef8b379c692a8cfc1.json
+export GCE_PROJECT=my-k8s-codelab-1384
