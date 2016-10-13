27cce22232109d2f06841d4c0e00808d34fc0d85
Yuming Ma <yumima@cisco.com>
2016-10-13 09:02:29 -0700

    add install ipaddress package

--- env.sh
+++ env.sh
@@ -1,4 +1,7 @@
-export GCE_EMAIL=712447643318-compute@developer.gserviceaccount.com
-export GCE_CREDENTIALS_FILE_PATH=/home/yumima/keys/3cbb462c342002e58105f68ef8b379c692a8cfc1.json
-export GCE_PROJECT=my-k8s-codelab-1384
+#export GCE_EMAIL=712447643318-compute@developer.gserviceaccount.com
+export GCE_EMAIL=staas-terraform@ccf-staas-cicd.iam.gserviceaccount.com
+#export GCE_CREDENTIALS_FILE_PATH=/home/yumima/keys/3cbb462c342002e58105f68ef8b379c692a8cfc1.json
+export GCE_CREDENTIALS_FILE_PATH=/home/yumima/keys/c6d7b17b35dc5b3254882e6133f85b536e0e50e0.json
+#export GCE_PROJECT=my-k8s-codelab-1384
+export GCE_PROJECT=ccf-staas-cicd
 export USER_HOME=/home/yumima
